3e4f6ba5e1c9c0583aeab4534ce01de5f24fab8b
Matthew Richards <mrichards30630@gmail.com>
2022-12-25 20:22:29 +1000

    rewrite to use bfs instead of dfs

--- sigourney.ml
+++ sigourney.ml
@@ -1,31 +1,75 @@
 #use "dictionary.ml";;
 open Dictionary;;
+open Hashtbl;;
 
 let distance_between (origin: string) (target: string) : int = 
   [0; 1; 2; 3] 
   |> List.map (fun n -> if (String.get origin n) = (String.get target n) then 0 else 1) 
   |> List.fold_left (+) 0;;
-  
+
 let find_adjacent_nodes (origin: string) : string list = 
   List.filter (fun word -> distance_between origin word = 1) Dictionary.four_letter_words;;
 
-let rec shortest_list (lst: 'a list list) : ('a list) option = 
-  List.fold_left (fun a b -> 
-    match a with
-      | Some x -> Some (if List.length x < List.length b then x else b)
-      | None -> Some b) 
-  None lst;;
+let contains target lst = 
+  List.exists (fun x -> x = target) lst;; 
 
-let rec find_path_between (origin: string) (target: string): string list option = 
-  let max_depth = 3 in
-  let rec aux (origin: string) (target: string) (path: string list) = 
-    if origin = target then Some (target :: path)
-    else if List.length path > max_depth - 1 then None
-    else origin 
-      |> find_adjacent_nodes 
-      |> List.filter (fun node -> List.for_all (fun e -> e <> node) path)
-      |> List.map (fun node -> aux node target (origin :: path))
-      |> List.filter (fun node -> Option.is_some node)
-      |> List.map (fun node -> Option.get node)
-      |> shortest_list
-  in aux origin target [];;
+module Queue = struct
+  type 'a queue = ('a list) ref;;
+
+  let empty: 'a queue = ref [];;
+
+  let enqueue (x: 'a) (q: 'a queue): unit = 
+    q := !q @ [x];;
+
+  let queue_of (lst: 'a list): 'a queue = 
+    let new_queue: 'a queue = empty in
+    let rec aux l = 
+      match l with 
+      | x :: xs -> enqueue x new_queue; aux xs
+      | [] -> new_queue in 
+    aux lst;;
+
+  let rec enqueue_all (lst: 'a list) (q: 'a queue): unit = 
+    match lst with 
+    | [] -> ()
+    | x :: xs -> 
+      enqueue x q; 
+      enqueue_all xs q;;
+
+  let dequeue (q: 'a queue): 'a option =
+    match !q with 
+    | [] -> None
+    | x :: xs -> 
+      q := xs; 
+      Some x;;
+
+  let is_empty (q: 'a queue): bool = 
+    List.length !q = 0;;
+end
+
+let rec backtrack (map: (string, string) Hashtbl.t) (initial: string) (target: string): string list =
+  let parent = Hashtbl.find map target in
+  if parent = initial then [initial]
+  else parent :: (backtrack map initial parent);;
+
+let bfs (initial: string) (target: string): string list = 
+  let queue = Queue.queue_of [initial] in
+  let visited: (string list) ref = ref [initial] in
+  let parent_map: (string, string) Hashtbl.t = Hashtbl.create 300 in
+  while not (Queue.is_empty queue) do 
+    match Queue.dequeue queue with
+    | Some t -> 
+      if t = target then 
+        queue := []
+      else
+        let adjs = find_adjacent_nodes t in
+        let filtered_adjs = List.filter (fun e -> not (contains e !visited)) adjs in
+        Queue.enqueue_all filtered_adjs queue;
+        visited := List.flatten [!visited; filtered_adjs];
+        List.fold_left (
+          fun a b -> 
+            Hashtbl.add parent_map b t
+        ) () filtered_adjs;
+    | None -> ()
+  done;
+  List.rev (target :: backtrack parent_map initial target);;
